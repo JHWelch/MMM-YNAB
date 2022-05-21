383b083548d15151ebf07123af49176d61e61300
Jordan Welch <jordanhwelch@gmail.com>
2022-05-21 17:14:43 -0500

    add waitForAsync helper

--- setupJest.js
+++ setupJest.js
@@ -1,6 +1,8 @@
+require('setimmediate');
+
 const { mockConsole, restoreConsole } = require('./__mocks__/console');
 
-beforeAll(mockConsole);
+global.waitForAsync = () => new Promise(resolve => setImmediate(resolve));
 
 afterEach(() => {
   jest.clearAllMocks();
